74d27ad12dc86e8eeb8aa4b64b3f0a8690d674ef
dung <dung@nextit.vn>
2026-03-06 12:02:31 +0700

    remove backend-nestjs

--- backend/src/users/dto/user.dto.ts
+++
@@ -1,8 +0,0 @@
-export class UserDto {
-  readonly email: string;
-  readonly password: string;
-  readonly fullName: string;
-  readonly phoneNumber: string;
-  readonly avatarUrl: string;
-  readonly fcmTokens: string[];
-}
